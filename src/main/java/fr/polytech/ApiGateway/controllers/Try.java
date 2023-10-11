7aecffce8d81ec66bdb0c8e87349eeea9e8b68c8
Salim-belkhir <salim.belkhir@etu.umontpellier.fr>
2023-10-11 16:50:28 +0200

    Configuration and dockerisation of the api gateway

--- src/main/java/fr/polytech/ApiGateway/controllers/Try.java
+++ src/main/java/fr/polytech/ApiGateway/controllers/Try.java
@@ -1,10 +1,14 @@
 package fr.polytech.ApiGateway.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/try")
 public class Try {
-    
+    @GetMapping("/hello")
+    public String index() {
+        return "Hello My friend 2!";
+    }
 }
